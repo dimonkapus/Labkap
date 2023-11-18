fb2e6718629886c71baabd642d582bf39a5ef297
kuta <zmk383@gmail.com>
2023-11-18 23:24:11 +0200

    -dz

--- t6.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    float deg_c;
-    printf("enter degrees in celsius:\n");
-    scanf("%f", &deg_c);
-    float deg_k = deg_c * 9.f / 5.f + 32.f;
-    printf("degrees in f: %g", deg_k);
-}
--- t8.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    float a, b;
-    scanf("%f%f", &a, &b);
-    printf("a-b = %.2f\na+b = %.2f\n", a - b, a * b);
-}

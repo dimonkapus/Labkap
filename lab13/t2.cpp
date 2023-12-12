70ccca9165022d1a8d0dc3a4f6a94c68e17fb37c
KAPUSTIAN DMYTRO <81609872+dimonkapus@users.noreply.github.com>
2023-12-12 14:21:17 +0200

    Kapustian Dmytro 2 year compmath1 lab13
    Classwork/Homework

---
+++ lab13/t2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <charconv>
+
+// Lab 12 task 2
+
+
+int main() {
+    std::string s;
+    std::getline(std::cin, s);
+    uint64_t sum;
+    for (size_t i = 0; i + 10 <= s.size(); i += 10) {
+        uint64_t num;
+        std::from_chars(s.data() + i, s.data() + i + 10, num);
+        sum += num;
+    }
+
+    std::cout << sum << '\n';
+}
